b01da55432595c5334d34725687fc6304362535e
a1yama <a1yama1123@gmail.com>
2016-12-21 01:36:35 +0900

    change table name

--- bin/dao/AnimeTitle.php
+++ bin/dao/AnimeTitle.php
@@ -5,7 +5,7 @@ class AnimeTitle extends Base
 {
     public function insert($row)
     {
-        $sql = "insert into anime_title ( ";
+        $sql = "insert into anime_titles ( ";
         $sql .= "title, company, created_at, updated_at) value (";
         $sql .= "?, ?, NOW(), NOW())";
 
--- bootstrap.php
+++ bootstrap.php
@@ -14,3 +14,6 @@ require __DIR__ . '/config/config.php';
 // container
 require_once __DIR__ . '/bin/Container.php';
 $container = Container::getContainer();
+$container['db.default'] = function ($c) {
+    return \Db\DbalFactory::mysql(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASS, 'utf8');
+};
